472618ef6234a409155340c27f0cfc0197cfe685
Michael Pacheco <mdpgd95@gmail.com>
2018-08-31 18:06:53 -0300

    first commit

--- src/br/ufop/decom/Application.java
+++ src/br/ufop/decom/Application.java
@@ -27,99 +27,100 @@ import interfaces.kernel.JCL_result;
  */
 public class Application {
 
-	public static void main(String[] args) {
-		JCL_facade jcl = JCL_FacadeImpl.getInstance();
+    public static void main(String[] args) {
+        JCL_facade jcl = JCL_FacadeImpl.getInstance();
 
-		Application.register(Sorter.class);
+        Application.register(Sorter.class);
 
-		Object[] sortArguments = {
-				// Number of generated random numbers
-				(int) 1e7,
+        Object[] sortArguments = {
+                // Number of generated random numbers
+                (int) 1e7,
 
-				// Lower bound (included)
-				0,
+                // Lower bound (included)
+                0,
 
-				// Upper bound (excluded)
-				10 };
+                // Upper bound (excluded)
+                10 };
 
-		// For all hosts generate random numbers and sort them
-		// But before we will start a timer.
-		long startTime = System.currentTimeMillis();
-		List<Future<JCL_result>> executeAll = jcl.executeAllCores(Sorter.class.getSimpleName(), "sort", sortArguments);
-		// Wait for all hosts end sorting
-		List<JCL_result> results = jcl.getAllResultBlocking(executeAll);
-		long endTime = System.currentTimeMillis();
-		long deltaTime = endTime - startTime;
-		System.out.println();
-		
-		Date date = new Date(deltaTime);
-		DateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
-		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-		System.out.println(String.format("The sorting was terminated! Total time: %s\n", formatter.format(date)));
+        // For all hosts generate random numbers and sort them
+        // But before we will start a timer.
+        long startTime = System.currentTimeMillis();
+        List<Future<JCL_result>> executeAll = jcl.executeAllCores(Sorter.class.getSimpleName(), "sort", sortArguments);
+        // Wait for all hosts end sorting
+        List<JCL_result> results = jcl.getAllResultBlocking(executeAll);
+        long endTime = System.currentTimeMillis();
+        long deltaTime = endTime - startTime;
+        System.out.println();
 
-		// Store the sorted collections returned by all hosts
-		final int numberOfHosts = jcl.getDevices().size();
-		
-		ArrayList<SortedMap<Integer, Long>> retrievedSortedMaps = new ArrayList<>(numberOfHosts);
+        Date date = new Date(deltaTime);
+        DateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        System.out.println(String.format("The sorting was terminated! Total time: %s\n", formatter.format(date)));
 
-		// Retrieve returned maps and check for errors
-		for (JCL_result jcl_result : results) {
-			if (jcl_result.getCorrectResult() == null)
-				jcl_result.getErrorResult().printStackTrace();
+        // Store the sorted collections returned by all hosts
+        final int numberOfHosts = jcl.getDevices().size();
 
-			@SuppressWarnings("unchecked")
-			SortedMap<Integer, Long> sortedMap = (SortedMap<Integer, Long>) jcl_result.getCorrectResult();
+        ArrayList<SortedMap<Integer, Long>> retrievedSortedMaps = new ArrayList<>(numberOfHosts);
 
-			retrievedSortedMaps.add(sortedMap);
-		}
+        // Retrieve returned maps and check for errors
+        for (JCL_result jcl_result : results) {
+            if (jcl_result.getCorrectResult() == null)
+                jcl_result.getErrorResult().printStackTrace();
 
-		Application.mergeAll(retrievedSortedMaps);
+            @SuppressWarnings("unchecked")
+            SortedMap<Integer, Long> sortedMap = (SortedMap<Integer, Long>) jcl_result.getCorrectResult();
 
-		Application.cleanupJCLandQuit(jcl, null, 0);
-	}
+            retrievedSortedMaps.add(sortedMap);
+        }
 
-	@SuppressWarnings("unused")
-	private static void printIndividualMap(SortedMap<Integer, Long> sortedMap) {
-		sortedMap.forEach((number, frequency) -> {
-			for (int i = 0; i < frequency; i++)
-				System.out.print(number + " ");
-		});
-		System.out.println();
-	}
+        Application.mergeAll(retrievedSortedMaps);
 
-	/**
-	 * Register a given class in JCL cluster and exit if there are errors.
-	 * 
-	 * @param toRegister the class to register
-	 */
-	private static void register(Class<?> toRegister) {
-		JCL_facade jcl = JCL_FacadeImpl.getInstance();
-		if (!jcl.register(toRegister, toRegister.getSimpleName()))
-			Application.cleanupJCLandQuit(jcl,
-					String.format("Failed to register \'%s.class\'", toRegister.getSimpleName()), 1);
-	}
+        Application.cleanupJCLandQuit(jcl, null, 0);
+    }
 
-	private static void cleanupJCLandQuit(JCL_facade jcl, String errorMessage, int returnCode) {
-		if (errorMessage != null)
-			System.err.println(errorMessage);
-		jcl.cleanEnvironment();
-		jcl.destroy();
-		System.exit(returnCode);
-	}
+    @SuppressWarnings("unused")
+    private static void printIndividualMap(SortedMap<Integer, Long> sortedMap) {
+        sortedMap.forEach((number, frequency) -> {
+            for (int i = 0; i < frequency; i++)
+                System.out.print(number + " ");
+        });
+        System.out.println();
+    }
 
-	private static SortedMap<Integer, Long> mergeAll(ArrayList<SortedMap<Integer, Long>> sortedMaps) {
-		SortedMap<Integer, Long> finalMap = new TreeMap<>();
+    /**
+     * Register a given class in JCL cluster and exit if there are errors.
+     * 
+     * @param toRegister
+     *            the class to register
+     */
+    private static void register(Class<?> toRegister) {
+        JCL_facade jcl = JCL_FacadeImpl.getInstance();
+        if (!jcl.register(toRegister, toRegister.getSimpleName()))
+            Application.cleanupJCLandQuit(jcl,
+                    String.format("Failed to register \'%s.class\'", toRegister.getSimpleName()), 1);
+    }
 
-		for (SortedMap<Integer, Long> sortedMap : sortedMaps) {
-			sortedMap.forEach((number, frequency) -> {
-				if (finalMap.containsKey(number))
-					finalMap.put(number, finalMap.get(number) + frequency);
-				else
-					finalMap.put(number, frequency);
-			});
-		}
+    private static void cleanupJCLandQuit(JCL_facade jcl, String errorMessage, int returnCode) {
+        if (errorMessage != null)
+            System.err.println(errorMessage);
+        jcl.cleanEnvironment();
+        jcl.destroy();
+        System.exit(returnCode);
+    }
 
-		return finalMap;
-	}
+    private static SortedMap<Integer, Long> mergeAll(ArrayList<SortedMap<Integer, Long>> sortedMaps) {
+        SortedMap<Integer, Long> finalMap = new TreeMap<>();
+
+        for (SortedMap<Integer, Long> sortedMap : sortedMaps) {
+            sortedMap.forEach((number, frequency) -> {
+                if (finalMap.containsKey(number))
+                    finalMap.put(number, finalMap.get(number) + frequency);
+                else
+                    finalMap.put(number, frequency);
+            });
+        }
+
+        return finalMap;
+    }
 
 }
